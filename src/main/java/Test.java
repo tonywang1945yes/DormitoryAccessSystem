d11270ce7c1dc2fe66b90706516537a0624610fa
Wen Sun <syl1887415157@126.com>
2018-12-12 18:45:18 +0800

    add mail and maven lib

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,7 +7,7 @@ import util.mailUtil.Mail;
 public class Test {
     public static void main(String[] args) {
         try {
-            Mail.sendMailWithAttachment("171250662@smail.nju.edu.cn", "MB6L7D9J6J8cQsoV", "595033456@qq.com",
+            Mail.sendMailWithAttachment("***@smail.nju.edu.cn", "*****", "595033456@qq.com",
                     "This is a test with an attachment.", "D:/test/test.xlsx");
         } catch (Exception ex) {
             ex.printStackTrace();
