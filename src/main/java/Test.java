29ed95dac871fc0d912ee47fcbdbc98878319cc4
Wen Sun <syl1887415157@126.com>
2018-12-14 11:02:39 +0800

    modified pom.xml and lib configuration

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,5 @@
+import util.excelUtil.ExcelReader;
+import util.excelUtil.Tutor;
 import util.mailUtil.Mail;
 
 /**
@@ -7,8 +9,11 @@ import util.mailUtil.Mail;
 public class Test {
     public static void main(String[] args) {
         try {
-            Mail.sendMailWithAttachment("***@smail.nju.edu.cn", "*****", "595033456@qq.com",
-                    "This is a test with an attachment.", "D:/test/test.xlsx");
+            Mail.sendMailWithAttachment("171250662@smail.nju.edu.cn", "***", "595033456@qq.com",
+                    "This is a test with an attachment.", "D:/test");
+            for (Tutor t : ExcelReader.readSimpleExcel("D:/test/测试数据.xlsx", "Sheet2")) {
+                System.out.println(t);
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
