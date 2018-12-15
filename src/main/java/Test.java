0c9d05b8a394338fcdaff372fc1bf4160b78b817
tony <wangruihua88@yahoo.com>
2018-12-15 09:30:48 +0800

    change

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,20 +2,21 @@ import util.excelUtil.ExcelReader;
 import util.excelUtil.Tutor;
 import util.mailUtil.Mail;
 
+import java.security.GeneralSecurityException;
+
 /**
  * @author Wen Sun
  * @date 2018/12/12
  */
 public class Test {
-    public static void main(String[] args) {
-        try {
-            Mail.sendMailWithAttachment("171250662@smail.nju.edu.cn", "***", "595033456@qq.com",
-                    "This is a test with an attachment.", "D:/test");
-            for (Tutor t : ExcelReader.readSimpleExcel("D:/test/测试数据.xlsx", "Sheet2")) {
-                System.out.println(t);
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+//    public static void main(String[] args) {
+//        try {
+//            Mail.sendMailWithAttachment("171250662@smail.nju.edu.cn", "***", "595033456@qq.com",
+//                    "This is a test with an attachment.", "D:/test");
+//            for (Tutor t : ExcelReader.readSimpleExcel("D:/test/测试数据.xlsx", "Sheet2")) {
+//                System.out.println(t);
+//            }
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
 }
